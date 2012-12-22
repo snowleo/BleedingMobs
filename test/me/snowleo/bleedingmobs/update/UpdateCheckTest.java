58ca8075712e128d46e69ba3a2d3c8b465b45694
snowleo <schneeleo@gmail.com>
2012-12-22 15:39:57 +0100

    Use existing data, so the test won't fail

--- test/me/snowleo/bleedingmobs/update/UpdateCheckTest.java
+++ test/me/snowleo/bleedingmobs/update/UpdateCheckTest.java
@@ -14,7 +14,7 @@ public class UpdateCheckTest
 	public void testUpdateCheck()
 	{
 		final UpdateCheck check = new UpdateCheck();
-		final VersionInfo info = check.checkForUpdate(new VersionString("4.0"), new VersionString("1.4.5-R0.2"));
+		final VersionInfo info = check.checkForUpdate(new VersionString("3.0"), new VersionString("1.3.1-R2.0"));
 		assertNotNull(info);
 		System.out.println(info.getVersion());
 		System.out.println(info.getGameVersions().iterator().next());
